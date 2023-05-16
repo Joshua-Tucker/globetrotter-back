0eabf899cd4d026dac121a9051961a6931d7e533
Josh Tucker <joshuaatucker@hotmail.com>
2023-05-16 21:29:45 +0100

    connected to mysql

--- src/main/java/org/globetrotter/Destination.java
+++ src/main/java/org/globetrotter/Destination.java
@@ -1,13 +1,11 @@
 package org.globetrotter;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "destination", schema = "globetrotter")
 public class Destination {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
